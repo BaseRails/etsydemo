421d94ab8c986bf307c54350c0c658a569668a55
Alex Yang <alex@baserails.com>
2013-12-30 19:55:03 -0800

    Typo

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,4 @@
 class Listing < ActiveRecord::Base
-  #
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
                     :storage => :dropbox,
                     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
