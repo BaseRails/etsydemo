20e7e6519272962990d0104aa6a0747801bffbc3
Alex Yang <alex@baserails.com>
2014-01-01 11:16:49 -0800

    Testing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,20 +1,34 @@
 class Listing < ActiveRecord::Base
-  paperclip_options = { :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg" }
+  # paperclip_options = { :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg" }
 
-  unless Rails.env.development?
-    paperclip_options.merge!  :storage => :dropbox,
-                              :path => ":style/:id_:filename",
-                              :dropbox_credentials => {
-                                app_key: "9s6t8tsrl0turoi",
-                                app_secret: ENV["APP_SECRET"],
-                                access_token: "189egb1nw6bjljr7",
-                                access_token_secret: ENV["ACCESS_TOKEN_SECRET"],
-                                user_id: "251697068",
-                                access_type: "app_folder"
-                              }
+  # unless Rails.env.development?
+  #   paperclip_options.merge!  :storage => :dropbox,
+  #                             :path => ":style/:id_:filename",
+  #                             :dropbox_credentials => {
+  #                               app_key: "9s6t8tsrl0turoi",
+  #                               app_secret: ENV["APP_SECRET"],
+  #                               access_token: "189egb1nw6bjljr7",
+  #                               access_token_secret: ENV["ACCESS_TOKEN_SECRET"],
+  #                               user_id: "251697068",
+  #                               access_type: "app_folder"
+  #                             }
+  # end
+
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+                      :storage => :dropbox,
+                      :path => ":style/:id_:filename",
+                      :dropbox_credentials => {
+                        app_key: "9s6t8tsrl0turoi",
+                        app_secret: ENV["APP_SECRET"],
+                        access_token: "189egb1nw6bjljr7",
+                        access_token_secret: ENV["ACCESS_TOKEN_SECRET"],
+                        user_id: "251697068",
+                        access_type: "app_folder"
+                      }
   end
-
-  has_attached_file :image, paperclip_options
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
