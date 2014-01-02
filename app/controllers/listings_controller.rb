88004975f99239a03227fece98be8d51019a6716
Alex Yang <alex@baserails.com>
2014-01-02 12:28:32 -0800

    Add Seller page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,16 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
+
+  def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
 
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Etsydemo::Application.routes.draw do
 
   get "pages/about"
   get "pages/contact"
+  get 'seller' => "listings#seller"
 
   root 'listings#index'
 
